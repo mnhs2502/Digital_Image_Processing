d7b26cdac417f31988a0a1622157e9a98665aa98
Md. Nujratul haque <haquemdn.eee@gmail.com>
2025-07-27 17:45:19 +0600

    Add files via upload

---
+++ PLT_Intensity_Level_Slicing_Suppress.m
@@ -0,0 +1,60 @@
+%% MATLAB code: Gray Level (Intensity Level) Slicing Transformation (Supressing Others)
+
+close all;
+clear all;
+clc
+
+%% Image Reading and Preprocessing
+
+% Read the image and convert it to grayscale
+OriginalImage = imread('woman.jpg');
+GrayImage = rgb2gray(OriginalImage);
+
+% Initialize output image matrix
+StretchedImage = 0;
+
+% Get size of the grayscale image
+[row, col] = size(GrayImage);
+
+%% Threshold Setup
+
+% Set the lower and upper intensity thresholds
+T1 = 100; % Lower threshold
+T2 = 180; % Upper threshold
+
+%% Gray Level Slicing Operation
+
+% Loop through each pixel and apply slicing transformation
+for i = 1:row
+    for j = 1:col
+        if (GrayImage(i,j) > T1) && (GrayImage(i,j) < T2)
+            StretchedImage(i,j) = 255;  % Highlight pixels within the range
+        else
+            StretchedImage(i,j) = 0;    % Suppress others
+        end
+    end
+end
+
+%% Plotting Transformation Curve
+
+curve = [];     % Initialize curve for plotting
+hold on;
+
+% Define the slicing curve manually
+curve(1:T1) = 0;
+curve(T1+1:T2-1) = 255;
+curve(T2:256) = 0;
+
+% Plot the transformation curve
+axis tight;
+plot(curve);
+xlabel('Intensity in input image');
+ylabel('Intensity in output image');
+title('Gray Level (Intensity) Slicing Transformation Function');
+
+%% Displaying the Result
+
+% Show original and transformed images side by side
+figure
+montage({GrayImage, StretchedImage}, 'Size', [1 2]);
+title('Original Image (Left) | Intensity Sliced Image (Right)');
